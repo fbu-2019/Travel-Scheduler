03f4f81ff45a500165d36d2eac41fe13478e6fb7
Angela Liu <aliu18@fb.com>
2019-07-16 09:21:38 -0700

    Added correct API manager.

--- Travel Scheduler/APIManager.h
+++ Travel Scheduler/APIManager.h
@@ -6,11 +6,11 @@
 //  Copyright © 2019 aliu18. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface APIManager : NSObject
+@interface APIManager : BDBOAuth1SessionManager
 
 @end
 
--- Travel Scheduler/APIManager.m
+++ Travel Scheduler/APIManager.m
@@ -8,14 +8,33 @@
 
 #import "APIManager.h"
 
+static NSString * const baseURLString = @"https://api.twitter.com";
+static NSString * const consumerKey = @"5lUJuO5AUpPUCez4ewYDFrtgh";// Enter your consumer key here
+
 @implementation APIManager
 
-#pragma mark - APIManager lifecycle
+#pragma mark - APIManager Creation
 
-//Gi
++ (instancetype)shared {
+    static APIManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
 
-//Franklin
+- (instancetype)init {
+    NSURL *baseURL = [NSURL URLWithString:baseURLString];
+    NSString *key = consumerKey;
+    NSString *secret = consumerSecret;
+    // Check for launch arguments override
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"]) {
+        key = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"];
+    }
+    self = [super initWithBaseURL:baseURL consumerKey:key consumerSecret:secret];
+    return self;
+}
 
-//Angela
 
 @end
