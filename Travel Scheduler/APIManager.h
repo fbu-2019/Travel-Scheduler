97716f2b1f5d826af6a31035fc0f58c75ef4cf69
Angela Liu <aliu18@fb.com>
2019-07-15 17:30:44 -0700

    Added gitignoer and gitattributes

---
+++ Travel Scheduler/APIManager.h
@@ -0,0 +1,17 @@
+//
+//  APIManager.h
+//  Travel Scheduler
+//
+//  Created by aliu18 on 7/15/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APIManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Travel Scheduler/APIManager.m
@@ -0,0 +1,13 @@
+//
+//  APIManager.m
+//  Travel Scheduler
+//
+//  Created by aliu18 on 7/15/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import "APIManager.h"
+
+@implementation APIManager
+
+@end
