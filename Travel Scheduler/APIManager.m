6fedc35b242667b83f3b58d76a4ffdacf3fe80b9
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-16 09:24:27 -0700

    wrote comments for the places where we can edit

--- Travel Scheduler/APIManager.m
+++ Travel Scheduler/APIManager.m
@@ -36,5 +36,16 @@ static NSString * const consumerKey = @"5lUJuO5AUpPUCez4ewYDFrtgh";// Enter your
     return self;
 }
 
+//Gi
 
+-(void)getLocation:(NSString *)location_name {
+    
+    NSString *apiRequestString =
+https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY
+}
+
+
+
+
+//Franklin
 @end
