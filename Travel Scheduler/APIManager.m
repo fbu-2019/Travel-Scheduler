b5946758d8d162b19e3b9ec1ae88a8011d4d49e8
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-16 17:11:22 -0700

    Wrote method to get distance from origin to destination

--- Travel Scheduler/APIManager.m
+++ Travel Scheduler/APIManager.m
@@ -139,4 +139,34 @@ static NSString * const consumerKey = @"AIzaSyC8Iz7AYw5g6mx1oq7bsVjbvLEPPKtrxik"
     }];
     [task resume];
 }
+
+-(void)getDistanceFromOrigin:(NSString *)origin toDestination:(NSString *)destination withCompletion:(void (^)(NSDictionary *distanceDurationDictionary, NSError *error))completion {
+    NSString *urlString = [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=%@&destinations=%@&key=%@",origin,destination,consumerKey];
+    
+    NSURL *url = [NSURL URLWithString:[urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        NSDictionary *jSONresult = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+        
+        if (error || [jSONresult[@"status"] isEqualToString:@"NOT_FOUND"] || [jSONresult[@"status"] isEqualToString:@"REQUEST_DENIED"]){
+            if (!error){
+                NSDictionary *userInfo = @{@"error":jSONresult[@"status"]};
+                NSError *newError = [NSError errorWithDomain:@"API Error" code:666 userInfo:userInfo];
+                completion(nil, newError);
+                return;
+            }
+            completion(nil, error);
+            return;
+        }
+        else {
+            NSDictionary *rowsDictionary = [jSONresult valueForKey:@"rows"];
+            NSDictionary *distanceDurationDictionary = rowsDictionary[@"elements"][0];
+            completion(distanceDurationDictionary, nil);
+        }
+    }];
+    [task resume];
+}
+
 @end
