3b339e09813b4ee6210f75a2a7cecebe2b75d275
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-17 10:26:30 -0700

    finished method to get list of places close to a hub (by name)

--- Travel Scheduler/View Controllers/Place.m
+++ Travel Scheduler/View Controllers/Place.m
@@ -14,17 +14,30 @@
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary {
     self = [super init];
     if(self) {
-    self.name = dictionary[@"name"];
-    self.address = dictionary[@"formatted_address"];
-    self.coordinates = dictionary[@"geometry"][@"location"];
-    self.iconUrl = dictionary[@"icon"];
-    self.placeId = dictionary[@"place_id"];
-    self.rating = dictionary[@"rating"];
-    self.photos = dictionary[@"photos"];
-    self.types = dictionary[@"types"];
-}
+        self.name = dictionary[@"name"];
+        self.address = dictionary[@"formatted_address"];
+        self.coordinates = dictionary[@"geometry"][@"location"];
+        self.iconUrl = dictionary[@"icon"];
+        self.placeId = dictionary[@"place_id"];
+        self.rating = dictionary[@"rating"];
+        self.photos = dictionary[@"photos"];
+        self.types = dictionary[@"types"];
+    }
     return self;
 }
+
+- (NSMutableArray *)placesWithArray:(NSArray *)arrayOfPlaceDictionaries {
+    
+    NSMutableArray *arrayOfPlaces = [NSMutableArray array];
+    
+    for (NSDictionary *dictionary in arrayOfPlaceDictionaries) {
+        Place *place = [[Place alloc] initWithDictionary:dictionary];
+        [arrayOfPlaces addObject:place];
+    }
+    
+    return arrayOfPlaces;
+}
+
 - (Place *)initWithName:(NSString *)name {
     __block Place *place = nil;
     [[APIManager shared]getCompleteInfoOfLocationWithName:name withCompletion:^(NSDictionary *placeInfoDictionary, NSError *error) {
@@ -37,5 +50,23 @@
         }
     }];
     return place;
+}
+
+- (NSArray *)getListOfPlacesCloseToPlaceWithName:(NSString *)centerPlaceName {
+    __block NSMutableArray *arrayOfPlaces = nil;
+    Place *hubPlace = [self initWithName:centerPlaceName];
+    NSString *hubLatitude = hubPlace.coordinates[@"lat"];
+    NSString *hubLongitude = hubPlace.coordinates[@"lng"];
+    
+    [[APIManager shared]getPlacesCloseToLatitude:hubLatitude andLongitude:hubLongitude withCompletion:^(NSArray *arrayOfPlacesDictionary, NSError *error) {
+        if(arrayOfPlacesDictionary) {
+            NSLog(@"Array of places dictionary worked");
+            arrayOfPlaces = [self placesWithArray:arrayOfPlacesDictionary];
+        }
+        else {
+            NSLog(@"did not work snif");
+        }
+    }];
+    return arrayOfPlaces;
 }
 @end
