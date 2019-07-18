c4ff426951ca99f23e29a74c1c3968ae1213d933
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-07-18 13:39:41 -0700

    Updated the textfield infor such that user can only select dates from today upwards and the minimum end date is that of the start date

--- Travel Scheduler/View Controllers/FirstScreenViewController.m
+++ Travel Scheduler/View Controllers/FirstScreenViewController.m
@@ -12,7 +12,7 @@
 
 @property(strong, nonatomic) UISearchBar *placesSearchBar;
 @property(strong, nonatomic) NSDateFormatter *dateFormat;
-@property(strong, nonatomic) UILabel *toSeperater;
+@property(strong, nonatomic) NSString *firstDateString;
 
 @end
 
@@ -23,38 +23,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    self.placesSearchBar.delegate = self;
+    [self setUpPlacesSearchBar];
+    [self.view addSubview:self.placesSearchBar];
+    [self setUpBeginDateText];
+    self.beginTripDateTextField.delegate = self;
+    [self.view addSubview:self.beginTripDateTextField];
+    [self setUpEndDateText];
+    self.endTripDateTextField.delegate = self;
+    [self.view addSubview:self.endTripDateTextField];
+}
+
+#pragma mark - Setting up search bar
+
+-(void)setUpPlacesSearchBar{
     CGRect screenFrame = self.view.frame;
     self.placesSearchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(2, CGRectGetHeight(screenFrame)/2, CGRectGetWidth(screenFrame) - 4, 50)];
-    self.placesSearchBar.delegate = self;
     self.placesSearchBar.autocorrectionType = UITextAutocorrectionTypeNo;
     self.placesSearchBar.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.placesSearchBar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     self.placesSearchBar.backgroundColor = [UIColor blackColor];
     self.placesSearchBar.placeholder = @"Search destination of choice ...";
-    [self.view addSubview:self.placesSearchBar];
+}
+
+#pragma mark - Setting up BeginDateTextField
+
+-(void)setUpBeginDateText{
+    CGRect screenFrame = self.view.frame;
     self.beginTripDateTextField = [[UITextField alloc] initWithFrame:CGRectMake(50, CGRectGetHeight(screenFrame)/1.7, 200, 50)];
     self.beginTripDateTextField.backgroundColor = [UIColor whiteColor];
+    self.beginTripDateTextField.text = nil;
     self.beginTripDateTextField.placeholder = @"Enter start date";
-    self.beginTripDateTextField.delegate = self;
-    [self.view addSubview:self.beginTripDateTextField];
     self.beginTripDatePicker = [[UIDatePicker alloc] init];
     [self.beginTripDatePicker setDate:[NSDate date]];
     self.beginTripDatePicker.datePickerMode = UIDatePickerModeDate;
     [self.beginTripDatePicker addTarget:self action:@selector(updateTextField :) forControlEvents:UIControlEventValueChanged];
     [self.beginTripDateTextField setInputView: self.beginTripDatePicker];
-    self.toSeperater = [[UILabel alloc] initWithFrame:CGRectMake(170, CGRectGetHeight(screenFrame)/1.7, 200, 45)];
-    self.toSeperater.backgroundColor = [UIColor whiteColor];
-    self.toSeperater.text = @"     to";
-    [self.view addSubview:self.toSeperater];
+    self.endTripDateTextField.text = nil;
+}
+
+#pragma mark - Setting up EndDateTextField
+
+-(void)setUpEndDateText{
+    CGRect screenFrame = self.view.frame;
     self.endTripDateTextField = [[UITextField alloc] initWithFrame:CGRectMake(245, CGRectGetHeight(screenFrame)/1.7, 200, 50)];
     self.endTripDateTextField.backgroundColor = [UIColor whiteColor];
     self.endTripDateTextField.textAlignment = NSTextAlignmentNatural;
+    self.endTripDateTextField.text = nil;
     self.endTripDateTextField.placeholder = @"Enter end date";
-    self.endTripDateTextField.delegate = self;
-    [self.view addSubview:self.endTripDateTextField];
     self.endTripDatePicker = [[UIDatePicker alloc] init];
     self.endTripDatePicker.datePickerMode = UIDatePickerModeDate;
-    [self.endTripDatePicker addTarget:self action:@selector(updateTextField :) forControlEvents:UIControlEventValueChanged];
+    [self.endTripDatePicker addTarget:self action:@selector(updateTextFieldEnd :) forControlEvents:UIControlEventValueChanged];
     [self.endTripDateTextField setInputView: self.endTripDatePicker];
 }
 
@@ -62,17 +81,22 @@
 
 -(void)updateTextField:(UIDatePicker *)sender
 {
-    self.endTripDateTextField.text = [self.dateFormat stringFromDate:sender.date];
-    [self.beginTripDatePicker setMaximumDate:[NSDate date]];
+    self.beginTripDatePicker.minimumDate = [NSDate date];
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-    NSDate *evenStartDate = self.beginTripDatePicker.date;
+    NSDate *eventStartDate = self.beginTripDatePicker.date;
+    [dateFormat setDateFormat:@"MM/dd/yyyy"];
+    NSString *dateString1 = [dateFormat stringFromDate:eventStartDate];
+    self.beginTripDateTextField.text = [NSString stringWithFormat:@"%@",dateString1];
+}
+
+-(void)updateTextFieldEnd:(UIDatePicker *)sender{
+    self.endTripDatePicker.minimumDate = [NSDate dateWithTimeInterval:1.0 sinceDate:self.beginTripDatePicker.date];
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     NSDate *eventEndDate = self.endTripDatePicker.date;
     [dateFormat setDateFormat:@"MM/dd/yyyy"];
-    NSString *dateString1 = [dateFormat stringFromDate:evenStartDate];
-    NSString *dateString2 = [dateFormat stringFromDate:eventEndDate];
-    self.beginTripDateTextField.text = [NSString stringWithFormat:@"%@",dateString1];
-    self.endTripDateTextField.text = [NSString stringWithFormat:@"%@",dateString2];
-    NSLog(@"%@", self.beginTripDateTextField.text);
+    NSString *dateString1 = [dateFormat stringFromDate:eventEndDate];
+    self.endTripDateTextField.text = [NSString stringWithFormat:@"%@",dateString1];
+    self.endTripDateTextField.text = [NSString stringWithFormat:@"%@",dateString1];
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -85,20 +109,18 @@
 
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
     if (searchText.length != 0) {
-        //place API stuff like autocomplete here
+        //TODO(Franklin): place API stuff like autocomplete here
     }
 }
 
 #pragma mark - UIPIckerView delegate methods
 
 - (NSInteger)numberOfComponentsInPickerView:(nonnull UIPickerView *)pickerView {
-    return 4;
+    return 3;
 }
 
 - (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
     return 4;
 }
-
-
 
 @end
