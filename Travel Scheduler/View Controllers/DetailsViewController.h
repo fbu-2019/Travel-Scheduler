25f04d41db4f0e5ce5e60ff479abef36af52183a
Angela Liu <aliu18@fb.com>
2019-07-17 22:44:40 -0700

    All segues working in home view controller. Not yet working in options one.

--- Travel Scheduler/View Controllers/DetailsViewController.h
+++ Travel Scheduler/View Controllers/DetailsViewController.h
@@ -7,13 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
-//#import "Place.h
+#import "Place.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DetailsViewController : UIViewController
 
-//@property (strong, nonatomic) Place *place;
+@property (strong, nonatomic) Place *place;
 
 @end
 
