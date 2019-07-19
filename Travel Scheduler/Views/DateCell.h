6a06aa963272516b19917b90d7cffbe03f17c1da
Angela Liu <aliu18@fb.com>
2019-07-18 22:21:33 -0700

    Added ScheduleView

---
+++ Travel Scheduler/Views/DateCell.h
@@ -0,0 +1,33 @@
+//
+//  DateCell.h
+//  Travel Scheduler
+//
+//  Created by aliu18 on 7/18/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol DateCellDelegate;
+
+@interface DateCell : UICollectionViewCell
+
+@property (strong, nonatomic) NSDate *date;
+@property (strong, nonatomic) UILabel *dayLabel;
+@property (weak, nonatomic) id<DateCellDelegate> delegate;
+
+- (void)makeDate:(NSDate *)date;
+- (void)setUnselected;
+- (void)setSelected;
+
+@end
+
+@protocol DateCellDelegate
+
+- (void)dateCell:(DateCell *) dateCell didTap: (NSDate *)date;
+
+@end
+
+NS_ASSUME_NONNULL_END
