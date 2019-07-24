b3c6ba5c0fa2cdf0b357ca2ebaca8f958872065d
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-24 13:43:53 -0700

    Put the collection view inside the table view cell. It is working now, but I still have to implement Angela's protocol

---
+++ Travel Scheduler/Helper Files/PlacesToVisitCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  PlacesToVisitCollectionView.h
+//  Travel Scheduler
+//
+//  Created by gilemos on 7/24/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlacesToVisitCollectionView : UICollectionView
+
+@property (nonatomic, strong) NSIndexPath *indexPath;
+
+@end
+
+NS_ASSUME_NONNULL_END
