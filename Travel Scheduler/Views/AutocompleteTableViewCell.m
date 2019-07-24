846b66b3f9d86b79c9d067f7fdb9cf0e5860d7e7
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-07-24 15:47:17 -0700

    Made Auto complete for First Page View controller

---
+++ Travel Scheduler/Views/AutocompleteTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  AutocompleteTableViewCell.m
+//  Travel Scheduler
+//
+//  Created by frankboamps on 7/24/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import "AutocompleteTableViewCell.h"
+
+@implementation AutocompleteTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
