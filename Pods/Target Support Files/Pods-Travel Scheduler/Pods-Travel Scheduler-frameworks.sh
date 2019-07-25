5ebf6f0b32849eef910a7612f8315457aa5bb428
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-25 16:35:03 -0700

    Added a pod

--- Pods/Target Support Files/Pods-Travel Scheduler/Pods-Travel Scheduler-frameworks.sh
+++ Pods/Target Support Files/Pods-Travel Scheduler/Pods-Travel Scheduler-frameworks.sh
@@ -164,11 +164,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BDBOAuth1Manager/BDBOAuth1Manager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GIFProgressHUD/GIFProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MFSideMenu/MFSideMenu.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BDBOAuth1Manager/BDBOAuth1Manager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GIFProgressHUD/GIFProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MFSideMenu/MFSideMenu.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
