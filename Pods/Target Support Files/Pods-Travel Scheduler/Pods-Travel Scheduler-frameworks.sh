5861fc42ee709230bef89abd627be0064cf116e4
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-25 08:36:44 -0700

    MADE LOADING SCREEN WORK

--- Pods/Target Support Files/Pods-Travel Scheduler/Pods-Travel Scheduler-frameworks.sh
+++ Pods/Target Support Files/Pods-Travel Scheduler/Pods-Travel Scheduler-frameworks.sh
@@ -164,11 +164,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BDBOAuth1Manager/BDBOAuth1Manager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MFSideMenu/MFSideMenu.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GIFProgressHUD/GIFProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BDBOAuth1Manager/BDBOAuth1Manager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MFSideMenu/MFSideMenu.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GIFProgressHUD/GIFProgressHUD.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
