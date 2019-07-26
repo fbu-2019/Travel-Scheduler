7e83ce28e0cd71639721039d674b7800ce9591c9
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-26 09:26:29 -0700

    Started new api call

--- Travel Scheduler/View Controllers/Place.h
+++ Travel Scheduler/View Controllers/Place.h
@@ -49,7 +49,6 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)initWithName:(NSString *)name beginHub:(bool)isHub;
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 - (void)setArrivalDeparture:(TimeBlock)timeBlock;
-- (void)setImageViewOfPlace:(Place *)myPlace withPriority:(bool)priority withDispatch:(dispatch_semaphore_t)setUpCompleted;
 
 @end
 
