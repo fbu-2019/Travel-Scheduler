d64d36459d3a2ab16acbb4bd89a2cb07c4b46990
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-31 16:33:49 -0700

    put the place for the map

--- Travel Scheduler/Views/DetailHeaderCell.m
+++ Travel Scheduler/Views/DetailHeaderCell.m
@@ -9,12 +9,14 @@
 #import "DetailHeaderCell.h"
 #import "Place.h"
 #import "UIImageView+AFNetworking.h"
+#import "QuartzCore/CALayer.h"
+#include <stdlib.h>
 
 #pragma mark - UI creation helpers
 
 static UILabel* makePlaceLabel(NSString *text, int width, CGRect imageFrame)
 {
-    UILabel *label = [[UILabel alloc]initWithFrame: CGRectMake(10, imageFrame.size.height - 65, width, 60)];
+    UILabel *label = [[UILabel alloc]initWithFrame: CGRectMake(10, imageFrame.size.height - 50, width, 45)];
     [label setFont: [UIFont fontWithName:@"Arial-BoldMT" size:25]];
     label.text = text;
     label.textColor = [UIColor whiteColor];
@@ -29,11 +31,12 @@ static UILabel* makePlaceLabel(NSString *text, int width, CGRect imageFrame)
     return label;
 }
 
-static UILabel* makeLocationLabel(NSString *text, CGRect labelFrame)
+static UILabel* makeLocationLabel(NSString *text, NSArray *arrayOfTypeLabels, int width)
 {
-    int xCoord = labelFrame.origin.x;
-    int yCoord = labelFrame.origin.y + CGRectGetHeight(labelFrame) + 10;
-    UILabel *label = [[UILabel alloc]initWithFrame: CGRectMake(xCoord, yCoord, CGRectGetWidth(labelFrame), 60)];
+    int xCoord = 10;
+    UILabel *lastTypeLabel = [arrayOfTypeLabels lastObject];
+    int yCoord = lastTypeLabel.frame.origin.y + lastTypeLabel.frame.size.height + 30;
+    UILabel *label = [[UILabel alloc]initWithFrame: CGRectMake(xCoord, yCoord, width/3, 60)];
     label.text = text;
     [label setFont: [UIFont fontWithName:@"Arial" size:20]];
     label.textColor = [UIColor grayColor];
@@ -42,20 +45,51 @@ static UILabel* makeLocationLabel(NSString *text, CGRect labelFrame)
     return label;
 }
 
-//static UILabel* makeDescriptionLabel(NSString *text, CGRect imageFrame, int width)
-//{
-//    int xCoord = imageFrame.origin.x;
-//    int yCoord = imageFrame.origin.y + CGRectGetHeight(imageFrame) + 25;
-//    int objectWidth = width - xCoord * 2;
-//    UILabel *label = [[UILabel alloc]initWithFrame: CGRectMake(xCoord, yCoord, objectWidth, 250)];
-//    label.text = text;
-//    [label setFont: [UIFont fontWithName:@"Arial" size:20]];
-//    label.textColor = [UIColor blackColor];
-//    [label setLineBreakMode:UILineBreakModeWordWrap];
-//    label.numberOfLines = 0;
-//    [label sizeToFit];
-//    return label;
-//}
+static UILabel* makeTypeLabel(NSString *type, int width, CGRect previousLabelFrame, NSArray *arrayOfColors)
+{
+    int xCoord;
+    int yCoord;
+    int LabelHeight = 25;
+    int labelWidth = 90;
+    int verticalSpacingBetweenLabels = 15;
+    int horizontalSpacingBetweenLabels = 10;
+    //Is the first label
+    if(previousLabelFrame.size.width > (width * 3) /4) {
+        yCoord = previousLabelFrame.origin.y + CGRectGetHeight(previousLabelFrame) + verticalSpacingBetweenLabels;
+        xCoord = width/2;
+    //Is not the first label
+    } else {
+        int expectedXCoord = previousLabelFrame.origin.x + previousLabelFrame.size.width + horizontalSpacingBetweenLabels;
+        //The previous label reached the end of the screen
+        if((expectedXCoord + labelWidth) > (width - 10)) {
+            //Go to the next line
+            xCoord = width/2;
+            yCoord = previousLabelFrame.origin.y + previousLabelFrame.size.height + verticalSpacingBetweenLabels;
+        //The previous label did not reach the end of the screen
+        } else {
+            xCoord = previousLabelFrame.origin.x + previousLabelFrame.size.width + horizontalSpacingBetweenLabels;
+            yCoord = previousLabelFrame.origin.y;
+        }
+    }
+    
+    UILabel *label = [[UILabel alloc]initWithFrame: CGRectMake(xCoord, yCoord, labelWidth, LabelHeight)];
+    label.text = type;
+    [label setFont: [UIFont fontWithName:@"Gotham-Light" size:12]];
+    label.numberOfLines = 1;
+    int randomColorIndex = arc4random_uniform((int)arrayOfColors.count - 1);
+    UIColor *randomColor = (UIColor *) arrayOfColors[randomColorIndex];
+    label.backgroundColor = randomColor;
+    label.textColor = [UIColor blackColor];
+    label.layer.masksToBounds = YES;
+    label.layer.cornerRadius = 5;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.layer.shadowColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1].CGColor;
+    label.layer.shadowOffset = CGSizeMake(0.0, 0.0);
+    label.layer.shadowRadius = 3.0;
+    label.layer.shadowOpacity = 1;
+    label.layer.shouldRasterize = YES;
+    return label;
+}
 
 static UIImageView* makeStarImageView(int starNumber, CGRect locationImageFrame, int width)
 {
@@ -71,12 +105,33 @@ static UIImageView* makeStarImageView(int starNumber, CGRect locationImageFrame,
 
 static UIImageView* makeSquareImage(int width)
 {
-//    int leftEdge = 15;
-//    int imageHeight = 175;
-//    int imageWidth = width/2 - leftEdge * 2;
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, width, (width * 3)/4)];
     imageView.contentMode = UIViewContentModeScaleAspectFill;
     imageView.clipsToBounds = YES;
+    return imageView;
+}
+
+static UIImageView* makeMapImageView(int width, NSArray *arrayOfTypeLabels)
+{
+    UILabel *lastTypeLabel = [arrayOfTypeLabels lastObject];
+    int verticalSpacingFromTypeLabel = 20;
+    int rightBorderSize = 20;
+    int yCoord = lastTypeLabel.frame.origin.y + lastTypeLabel.frame.size.height + verticalSpacingFromTypeLabel;
+    int xCoord = width / 2;
+    int sidesSize = width/2 - rightBorderSize;
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(xCoord, yCoord, sidesSize, sidesSize)];
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    imageView.layer.shadowColor = [UIColor blackColor].CGColor;
+    imageView.layer.shadowOffset = CGSizeMake(0, 1);
+    imageView.layer.shadowOpacity = 1;
+    imageView.layer.shadowRadius = 3.0;
+    imageView.clipsToBounds = NO;
+    
+    imageView.layer.masksToBounds = YES;
+    imageView.layer.borderColor = [UIColor blackColor].CGColor;
+    imageView.layer.borderWidth = 1;
+    
+    //  imageView.clipsToBounds = YES;
     return imageView;
 }
 
@@ -120,6 +175,7 @@ static void setButtonState(UIButton *button, Place *place)
 - (instancetype)initWithWidth:(int)width andPlace:(Place *)givenPlace
 {
     self = [super init];
+    [self makeArrayOfColors];
     self.place = givenPlace;
     self.width = width;
     self.contentView.backgroundColor = [UIColor whiteColor];
@@ -128,10 +184,30 @@ static void setButtonState(UIButton *button, Place *place)
     self.placeNameLabel = [[UILabel alloc] initWithFrame:CGRectZero];
     self.goingButton = [[UIButton alloc] initWithFrame:CGRectZero];
     self.arrayOfStarImageViews = [[NSMutableArray alloc] init];
+    self.arrayOfTypeLabels = [[NSMutableArray alloc] init];
     [self customLayouts];
     return self;
 }
+    
+- (void)makeArrayOfTypeLabels
+{
+    CGRect previousLabelFrame = self.image.frame;
+    for(NSString *type in self.place.types) {
+        if([type isEqualToString:@"point_of_interest"]) {
+            continue;
+        }
+        UILabel *curLabel = makeTypeLabel(type, self.width, previousLabelFrame, self.colorArray);
+        [self.arrayOfTypeLabels addObject:curLabel];
+        previousLabelFrame = curLabel.frame;
+        [self.contentView addSubview:curLabel];
+    }
+}
 
+- (void)makeArrayOfColors
+{
+    self.colorArray = [NSArray alloc];
+    self.colorArray = @[[UIColor colorWithRed:0.33 green:0.94 blue:0.77 alpha:1.0], [UIColor colorWithRed:0.51 green:0.93 blue:0.93 alpha:1.0], [UIColor colorWithRed:0.45 green:0.73 blue:1.00 alpha:1.0], [UIColor colorWithRed:0.64 green:0.61 blue:1.00 alpha:1.0], [UIColor colorWithRed:0.00 green:0.72 blue:0.58 alpha:1.0], [UIColor colorWithRed:0.00 green:0.81 blue:0.79 alpha:1.0], [UIColor colorWithRed:0.04 green:0.52 blue:0.89 alpha:1.0], [UIColor colorWithRed:0.42 green:0.36 blue:0.91 alpha:1.0], [UIColor colorWithRed:0.98 green:0.69 blue:0.63 alpha:1.0], [UIColor colorWithRed:1.00 green:0.46 blue:0.46 alpha:1.0], [UIColor colorWithRed:0.99 green:0.47 blue:0.66 alpha:1.0], [UIColor colorWithRed:0.99 green:0.80 blue:0.43 alpha:1.0], [UIColor colorWithRed:0.88 green:0.44 blue:0.33 alpha:1.0], [UIColor colorWithRed:0.84 green:0.19 blue:0.19 alpha:1.0], [UIColor colorWithRed:0.91 green:0.26 blue:0.58 alpha:1.0]];
+}
 - (void)customLayouts
 {
     self.image = makeSquareImage(self.width);
@@ -141,8 +217,6 @@ static void setButtonState(UIButton *button, Place *place)
     self.placeNameLabel = makePlaceLabel(self.place.name, self.width, self.image.frame);
     [self.image addSubview:self.placeNameLabel];
     
-    self.locationLabel = makeLocationLabel(self.place.address, self.placeNameLabel.frame);
-    [self.contentView addSubview:self.locationLabel];
     int rating = [self.place.rating intValue];
     for(int i = 1; i <= rating; ++i) {
         if(self.arrayOfStarImageViews == nil) {
@@ -152,11 +226,22 @@ static void setButtonState(UIButton *button, Place *place)
         [self.arrayOfStarImageViews addObject:curImageView];
         [self.contentView addSubview:curImageView];
     }
+    
+    [self makeArrayOfTypeLabels];
+    
+    self.locationLabel = makeLocationLabel(self.place.address, self.arrayOfTypeLabels, self.width);
+    [self.contentView addSubview:self.locationLabel];
+    
+    self.mapImageView = makeMapImageView(self.width, self.arrayOfTypeLabels);
+    self.mapImageView.image = [UIImage imageNamed:@"3405110-512"];
+    [self.contentView addSubview:self.mapImageView];
+    
     self.goingButton = makeGoingButton(@"Not going", self.image, self.locationLabel, self.width);
     setButtonState(self.goingButton, self.place);
     [self.goingButton addTarget:self action:@selector(selectPlace) forControlEvents:UIControlEventTouchUpInside];
     [self.contentView addSubview:self.goingButton];
-    int height = self.image.frame.origin.y + CGRectGetHeight(self.image.frame) + 30 + 25;
+    
+    int height = self.mapImageView.frame.origin.y + self.mapImageView.frame.size.height + 30 + 25;
     self.contentView.frame = CGRectMake(0, 0, self.width, height);
 }
 
