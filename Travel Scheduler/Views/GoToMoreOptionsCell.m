15504469ca786a8eda95ee36a49e695fd46fa277
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-08-02 14:15:38 -0700

    Finished the design of the cell

--- Travel Scheduler/Views/GoToMoreOptionsCell.m
+++ Travel Scheduler/Views/GoToMoreOptionsCell.m
@@ -14,11 +14,20 @@
 static void instantiateImageView(UIImageView *imageView)
 {
     imageView.contentMode = UIViewContentModeScaleAspectFill;
-    imageView.layer.cornerRadius = 30;
+    imageView.layer.cornerRadius = 5;
     imageView.clipsToBounds = YES;
     [imageView.layer setBorderColor: [[UIColor whiteColor] CGColor]];
-    imageView.image = [UIImage imageNamed:@"iconfinder_104_Direction_Board_Camping_Sign_label_camping_camp_4172884"];
-    imageView.alpha = 0.5;
+    imageView.image = [UIImage imageNamed:@"2103641-512 (1)"];
+}
+
+static void instantiateTitle(UILabel *titleLabel)
+{
+    [titleLabel setFont: [UIFont fontWithName:@"Gotham-Bold" size:17]];
+    titleLabel.text = @"MORE";
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.numberOfLines = 1;
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.layer.shouldRasterize = YES;
 }
 
 @implementation GoToMoreOptionsCell
@@ -32,27 +41,39 @@ static void instantiateImageView(UIImageView *imageView)
     if(self.imageView == nil) {
     self.imageView = [[UIImageView alloc] init];
     }
+    
+    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,10,self.contentView.bounds.size.width ,self.contentView.frame.size.height/5)];
+    instantiateTitle(self.titleLabel);
+    [self.contentView addSubview:self.titleLabel];
+    
+    
     instantiateImageView(self.imageView);
+    self.imageView.frame = CGRectMake(self.contentView.frame.size.width/4,self.titleLabel.frame.origin.y  + self.titleLabel.frame.size.height + 10,self.contentView.bounds.size.width/2,self.contentView.bounds.size.height/2);
     [self.contentView addSubview:self.imageView];
     [self instantiateGestureRecognizers];
 }
-    
-- (void)layoutSubviews
-{
-    [super layoutSubviews];
-    self.imageView.frame = CGRectMake(0,0,self.contentView.bounds.size.width,self.contentView.bounds.size.height);
-}
+
 
 #pragma mark - General helper methods
 
 -(void)setShading
 {
+    UIColor *pinkColor = [UIColor colorWithRed:0.93 green:0.30 blue:0.40 alpha:1];
+    
     self.layer.shadowOffset = CGSizeMake(1, 0);
     self.layer.shadowColor = [[UIColor blackColor] CGColor];
     self.layer.shadowRadius = 5;
-    self.layer.shadowOpacity = .50;
+    self.layer.shadowOpacity = .5;
     self.clipsToBounds = false;
     self.layer.masksToBounds = false;
+    self.layer.borderColor = [[UIColor grayColor] CGColor];
+    self.layer.borderWidth = 2;
+    self.layer.masksToBounds = false;
+    
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.bounds;
+    gradient.colors = @[(id)pinkColor.CGColor, (id)[UIColor lightGrayColor].CGColor];
+    [self.layer insertSublayer:gradient atIndex:0];
 }
 #pragma mark - Segue to more options view controller
 
